023cf5ad732dbaa0518a08c2946bebc7011c3637
Anzumana <anzumana.taal@me.com>
2016-01-17 00:40:35 +0100

    hover works for pics :)

--- animations.js
+++ animations.js
@@ -3,9 +3,49 @@
 $( document ).ready(function() {
 	console.log( "ready!" );
 	$('.shape').shape();
-	$('#test').click(function(){
-		console.log("testts");
-		$('.shape').shape();
-		$('.shape').shape('flip up');
+	$('#test0').mouseenter(function(){ 
+		$('#test0').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test0').shape('flip back');
+	}
+	);
+	$('#test1').mouseenter(function(){ 
+		$('#test1').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test1').shape('flip back');
+	}
+	);
+	$('#test2').mouseenter(function(){ 
+		$('#test2').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test2').shape('flip back');
+	}
+	);
+	$('#test3').mouseenter(function(){ 
+		$('#test3').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test3').shape('flip back');
+	}
+	);
+	$('#test4').mouseenter(function(){ 
+		$('#test4').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test4').shape('flip back');
+	}
+	);
+	$('#test5').mouseenter(function(){ 
+		$('#test5').shape('flip back');
+	}
+	).mouseleave(function(){ 
+		$('#test5').shape('flip back');
+	}
+	);
+
+	$('.shape').click(function(){
 		});
 });
